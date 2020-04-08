3f7902abbe5403eef7943426e683189898cb44ad
Joe Horacki <prince_937@hotmail.com>
2020-04-08 08:27:54 -0600

    Lecture 50 - Coding the Basic Game Loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,22 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Cows and Bulls!"));
-    PrintLine(TEXT("Please type something and press enter"));
+    PrintLine(TEXT("Guess the 4 letter word"));  //Magic number remove
+    PrintLine(TEXT("Press Enter to continue...."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("oneway");
+    FString HiddenWord = TEXT("oneway"); // Move outside this function
     PrintLine(Input);
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have Won!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You have Lost!"));
+    }
+    
 }
